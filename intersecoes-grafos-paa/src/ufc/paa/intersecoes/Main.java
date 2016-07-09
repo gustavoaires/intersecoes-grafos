07aaf05cc1e46734270ea26737cf24b608cd2579
gustavoaires <gustavoairesmatos@gmail.com>
2016-07-09 16:12:36 -0300

    Utilizando lista no lugar de matriz de adjacência.

--- intersecoes-grafos-paa/src/ufc/paa/intersecoes/Main.java
+++ intersecoes-grafos-paa/src/ufc/paa/intersecoes/Main.java
@@ -1,5 +1,7 @@
 package ufc.paa.intersecoes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,17 +12,32 @@ public class Main {
 		n = e.nextInt();
 		m = e.nextInt();
 		//os labels dos vertices comecam de 1
-		int grafo[][] =  new int[n+1][n+1];
+		ArrayList<ArrayList<Integer>> grafo = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i <= n; i++)
+			grafo.get(i).add(null);
 
 		for (int i = 1; i <= m; i++) {
 			v = e.nextInt();
 			w = e.nextInt();
 			p = e.nextInt();
 			//caso p seja 1, somente essa linha sera executada (tem uma aresta de v para w)
-			grafo[v][w] = 1;
+			if (grafo.get(v) != null) {
+				grafo.get(v).add(w);
+			} else {
+				ArrayList<Integer> a = new ArrayList<Integer>();
+				a.add(w);
+				grafo.add(v,a);
+			}
 			//se a entrada de p for 2, havera aresta de ida e volta (o if contempla isso)
-			if (p == 2)
-				grafo[w][v] = 1;
+			if (p == 2) {
+				if (grafo.get(w) != null) {
+					grafo.get(w).add(v);
+				} else {
+					ArrayList<Integer> a = new ArrayList<Integer>();
+					a.add(v);
+					grafo.add(w,a);
+				}
+			}
 		}
 		System.out.println(/*nome do metodo mapeamento intersecoes*/);
 		e.close();
